8354b20915ac9039b3c65d8209a0d0dc3b676875
Gracia-Gwati <gracia.gwati@gmail.com>
2024-11-09 16:14:23 +0200

    Created the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React, { useState } from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
+  const [city, setCity] = useState("");
+
+  const handleSearch = (event) => {
+    setCity(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Weather app</p>
-      </header>
+    <div className="app">
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        placeholder="Enter city name"
+        onChange={handleSearch}
+      />
+      <Weather city={city} />
     </div>
   );
 }
